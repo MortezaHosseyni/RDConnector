89f70da3d1c6814c4e10f178901ad113569e849d
MortezaHosseyni <morteza_hosseini_dev@outlook.com>
2024-02-28 20:01:31 +0330

    Main Form UI Create application and set first configuration Create Main Form UI Create Gitignore Import assets

---
+++ RDConnector/frmMain.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace RDConnector
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pnl_Header = new System.Windows.Forms.Panel();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Minimized = new System.Windows.Forms.Button();
+            this.lbol_Title = new System.Windows.Forms.Label();
+            this.lbx_ServerList = new System.Windows.Forms.ListBox();
+            this.btn_LoadServer = new System.Windows.Forms.Button();
+            this.btn_ExportList = new System.Windows.Forms.Button();
+            this.lbl_ComingSoon = new System.Windows.Forms.Label();
+            this.btn_About = new System.Windows.Forms.Button();
+            this.cms_ServerListFunctions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.connectToServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.pingTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnl_Header.SuspendLayout();
+            this.cms_ServerListFunctions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Header
+            // 
+            this.pnl_Header.BackColor = System.Drawing.Color.ForestGreen;
+            this.pnl_Header.Controls.Add(this.btn_About);
+            this.pnl_Header.Controls.Add(this.lbol_Title);
+            this.pnl_Header.Controls.Add(this.btn_Minimized);
+            this.pnl_Header.Controls.Add(this.btn_Exit);
+            this.pnl_Header.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Header.Name = "pnl_Header";
+            this.pnl_Header.Size = new System.Drawing.Size(800, 31);
+            this.pnl_Header.TabIndex = 0;
+            this.pnl_Header.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_Header_MouseDown);
+            this.pnl_Header.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnl_Header_MouseMove);
+            this.pnl_Header.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnl_Header_MouseUp);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Red;
+            this.btn_Exit.ForeColor = System.Drawing.Color.White;
+            this.btn_Exit.Location = new System.Drawing.Point(769, 0);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(31, 31);
+            this.btn_Exit.TabIndex = 1;
+            this.btn_Exit.Text = "❌";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // btn_Minimized
+            // 
+            this.btn_Minimized.BackColor = System.Drawing.Color.Blue;
+            this.btn_Minimized.ForeColor = System.Drawing.Color.White;
+            this.btn_Minimized.Location = new System.Drawing.Point(732, 0);
+            this.btn_Minimized.Name = "btn_Minimized";
+            this.btn_Minimized.Size = new System.Drawing.Size(31, 31);
+            this.btn_Minimized.TabIndex = 2;
+            this.btn_Minimized.Text = "⛔️";
+            this.btn_Minimized.UseVisualStyleBackColor = false;
+            // 
+            // lbol_Title
+            // 
+            this.lbol_Title.AutoSize = true;
+            this.lbol_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbol_Title.ForeColor = System.Drawing.Color.GhostWhite;
+            this.lbol_Title.Location = new System.Drawing.Point(5, 6);
+            this.lbol_Title.Name = "lbol_Title";
+            this.lbol_Title.Size = new System.Drawing.Size(111, 20);
+            this.lbol_Title.TabIndex = 1;
+            this.lbol_Title.Text = "RD Connector";
+            // 
+            // lbx_ServerList
+            // 
+            this.lbx_ServerList.BackColor = System.Drawing.Color.Black;
+            this.lbx_ServerList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbx_ServerList.ForeColor = System.Drawing.Color.Lime;
+            this.lbx_ServerList.FormattingEnabled = true;
+            this.lbx_ServerList.ItemHeight = 16;
+            this.lbx_ServerList.Location = new System.Drawing.Point(12, 78);
+            this.lbx_ServerList.Name = "lbx_ServerList";
+            this.lbx_ServerList.Size = new System.Drawing.Size(295, 340);
+            this.lbx_ServerList.TabIndex = 1;
+            // 
+            // btn_LoadServer
+            // 
+            this.btn_LoadServer.BackColor = System.Drawing.Color.Black;
+            this.btn_LoadServer.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_LoadServer.Location = new System.Drawing.Point(12, 46);
+            this.btn_LoadServer.Name = "btn_LoadServer";
+            this.btn_LoadServer.Size = new System.Drawing.Size(136, 26);
+            this.btn_LoadServer.TabIndex = 2;
+            this.btn_LoadServer.Text = "Load Server List";
+            this.btn_LoadServer.UseVisualStyleBackColor = false;
+            // 
+            // btn_ExportList
+            // 
+            this.btn_ExportList.BackColor = System.Drawing.Color.Black;
+            this.btn_ExportList.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_ExportList.Location = new System.Drawing.Point(154, 46);
+            this.btn_ExportList.Name = "btn_ExportList";
+            this.btn_ExportList.Size = new System.Drawing.Size(153, 26);
+            this.btn_ExportList.TabIndex = 3;
+            this.btn_ExportList.Text = "Export List";
+            this.btn_ExportList.UseVisualStyleBackColor = false;
+            // 
+            // lbl_ComingSoon
+            // 
+            this.lbl_ComingSoon.AutoSize = true;
+            this.lbl_ComingSoon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbl_ComingSoon.ForeColor = System.Drawing.Color.GhostWhite;
+            this.lbl_ComingSoon.Location = new System.Drawing.Point(517, 209);
+            this.lbl_ComingSoon.Name = "lbl_ComingSoon";
+            this.lbl_ComingSoon.Size = new System.Drawing.Size(117, 20);
+            this.lbl_ComingSoon.TabIndex = 3;
+            this.lbl_ComingSoon.Text = "Coming Soon...";
+            // 
+            // btn_About
+            // 
+            this.btn_About.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btn_About.ForeColor = System.Drawing.Color.White;
+            this.btn_About.Location = new System.Drawing.Point(693, 0);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(31, 31);
+            this.btn_About.TabIndex = 3;
+            this.btn_About.Text = "❗️";
+            this.btn_About.UseVisualStyleBackColor = false;
+            // 
+            // cms_ServerListFunctions
+            // 
+            this.cms_ServerListFunctions.BackColor = System.Drawing.Color.Black;
+            this.cms_ServerListFunctions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToServerToolStripMenuItem,
+            this.statusToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.pingTestToolStripMenuItem});
+            this.cms_ServerListFunctions.Name = "cms_ServerListFunctions";
+            this.cms_ServerListFunctions.Size = new System.Drawing.Size(170, 76);
+            // 
+            // connectToServerToolStripMenuItem
+            // 
+            this.connectToServerToolStripMenuItem.ForeColor = System.Drawing.Color.Lime;
+            this.connectToServerToolStripMenuItem.Name = "connectToServerToolStripMenuItem";
+            this.connectToServerToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.connectToServerToolStripMenuItem.Text = "Connect To Server";
+            // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.ForeColor = System.Drawing.Color.Lime;
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.statusToolStripMenuItem.Text = "Status";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(166, 6);
+            // 
+            // pingTestToolStripMenuItem
+            // 
+            this.pingTestToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pingTestToolStripMenuItem.Name = "pingTestToolStripMenuItem";
+            this.pingTestToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.pingTestToolStripMenuItem.Text = "Ping Test";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_ComingSoon);
+            this.Controls.Add(this.btn_ExportList);
+            this.Controls.Add(this.btn_LoadServer);
+            this.Controls.Add(this.lbx_ServerList);
+            this.Controls.Add(this.pnl_Header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RD Connector";
+            this.pnl_Header.ResumeLayout(false);
+            this.pnl_Header.PerformLayout();
+            this.cms_ServerListFunctions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Header;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Minimized;
+        private System.Windows.Forms.Label lbol_Title;
+        private System.Windows.Forms.ListBox lbx_ServerList;
+        private System.Windows.Forms.Button btn_LoadServer;
+        private System.Windows.Forms.Button btn_ExportList;
+        private System.Windows.Forms.Label lbl_ComingSoon;
+        private System.Windows.Forms.Button btn_About;
+        private System.Windows.Forms.ContextMenuStrip cms_ServerListFunctions;
+        private System.Windows.Forms.ToolStripMenuItem connectToServerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pingTestToolStripMenuItem;
+    }
+}
+
---
+++ RDConnector/frmMain.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace RDConnector
+{
+    public partial class frmMain : Form
+    {
+        private Point offset;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Header
+        private void pnl_Header_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                offset = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pnl_Header_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point newLocation = this.PointToScreen(new Point(e.X, e.Y));
+                newLocation.Offset(-offset.X, -offset.Y);
+                this.Location = newLocation;
+            }
+        }
+
+        private void pnl_Header_MouseUp(object sender, MouseEventArgs e)
+        {
+            offset = Point.Empty;
+        }
+        #endregion
+
+
+    }
+}
